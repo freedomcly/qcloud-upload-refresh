dbddfba5ce0bb6e6a94aab978fe36ce8d97525c7
cuilingyun <cuilingyun@zhongan.com>
2018-04-02 15:44:33 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,43 @@
+qcloud upload and refresh.
+
+# 安装
+
+    npm install qcloud-upload-refresh
+
+# 初始化
+
+首先，在项目根目录下新建文件`cdn.config.js`
+
+    {
+        "bucket": "",
+        "region": "",
+        "secretId": "",
+        "secretKey": "",
+        "cdnPath": "",
+        "cdnDomain": "",
+        "sourcePath": ""
+    }
+
+bucket、region、secretId、secretKey可以在腾讯云后台获取。
+cdnPath：上传到bucket中的文件路径。
+cdnDomain：cdn域名。
+sourcePath：需要上传的文件夹相对路径。如：src/modules/assets。
+上传到cdn后，文件访问路径就是：{cdnDomain}/{cdnPath}/{fileName}。
+
+然后，在`package.json`中写入
+
+    {
+        "scripts": {
+            "cdn": "node ./node_modules/qcloud-upload-refresh/index.js"
+        }
+    }
+
+完成配置。
+
+# 使用
+
+每次上传cdn时，运行：
+
+    npm run cdn
+
+上传到cdn的文件会立即执行url刷新。
---
+++ index.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const dir = require('node-dir')
+const cosSdk = require('cos-nodejs-sdk-v5')
+const qcloudSDK = require('qcloud-cdn-node-sdk')
+const cdnConfig = require('../../cdn.config.json')
+
+const {bucket, region, secretId, secretKey, cdnPath, cdnDomain, sourcePath} = cdnConfig
+const base = path.resolve(__dirname, `../../${sourcePath}`)
+
+const cos = new cosSdk({
+  SecretId: secretId,
+  SecretKey: secretKey
+})
+
+qcloudSDK.config({secretId, secretKey})
+
+dir.files(base, (err, files) => {
+  if (err) return console.error(err)
+  return files.forEach((file, index) => {
+    // for cross platform: windows and linux
+    const key = file.replace(base, cdnPath).split(path.sep).join('/')
+    const tempArr = key.split('/')
+    const fileName = tempArr[tempArr.length - 1]
+
+    cos.sliceUploadFile({
+      Bucket: bucket,
+      Region: region,
+      Key: key,
+      FilePath: file
+    }, (err) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+
+      qcloudSDK.request('RefreshCdnUrl', {
+        'urls.0': `${cdnDomain}/${cdnPath}/${fileName}`,
+      }, (res) => {
+        console.log('cdn upload and refresh success')
+        console.log(`${cdnDomain}/${cdnPath}/${fileName}`)
+      })
+    })
+  })
+})
